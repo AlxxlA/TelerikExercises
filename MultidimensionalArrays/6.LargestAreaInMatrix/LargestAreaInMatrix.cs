3da39fd795475a3c7f4fb096f728cd84450b27a6
Alexander Markov <alexander_29@abv.bg>
2017-11-26 17:50:44 +0200

    Solved Bishop Path Finder.

---
+++ MultidimensionalArrays/6.LargestAreaInMatrix/LargestAreaInMatrix.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.LargestAreaInMatrix
+{
+    class LargestAreaInMatrix
+    {
+        static void Main()
+        {
+            string[] dimensions = Console.ReadLine().Split();
+
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            FillMatrix(matrix);
+
+            int count = DFS(matrix, matrix[0, 0]);
+
+            Console.WriteLine(count);
+
+            PrintMatrix(matrix);
+
+            string s = "asdsad";
+            
+
+            
+        }
+
+        private static int DFS(int[,] matrix, int previousElement, int row = 0, int col = 0, int count = 0, HashSet<Position> visited = null)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            if (row < 0 || row >= rows || col < 0 || col >= cols)
+            {
+                return count;
+            }
+            int currentElement = matrix[row, col];
+            if (currentElement != previousElement)
+            {
+                return count;
+            }
+
+            Position currentPosition = new Position(row, col);
+            if (visited == null)
+            {
+                visited = new HashSet<Position>();
+            }
+            if (visited.Contains(currentPosition))
+            {
+                return count;
+            }
+            //visited.Add(currentPosition);
+
+            count++;
+
+            for (int i = row; i < rows; i++)
+            {
+                for (int j = col; j < cols; j++)
+                {
+                    currentElement = matrix[i, j];
+                    visited.Add(new Position(i,j));
+                    DFS(matrix, currentElement, i - 1, j, count, visited);
+                    DFS(matrix, currentElement, i + 1, j, count, visited);
+                    DFS(matrix, currentElement, i, j - 1, count, visited);
+                    DFS(matrix, currentElement, i, j + 1, count, visited);
+                }
+            }
+            //DFS(matrix, currentElement, row - 1, col, count, visited);
+            //DFS(matrix, currentElement, row + 1, col, count, visited);
+            //DFS(matrix, currentElement, row, col - 1, count, visited);
+            //DFS(matrix, currentElement, row, col + 1, count, visited);
+
+
+
+
+            return count;
+
+        }
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] nums = Console.ReadLine().Split();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(nums[j]);
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string row = string.Empty;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row += matrix[i, j] + " ";
+                }
+                Console.WriteLine(row.Trim());
+            }
+        }
+    }
+
+    struct Position
+    {
+
+        public Position(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
---
+++ MultidimensionalArrays/7.BishopPathFinder/BishopPathFinder.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace _7.BishopPathFinder
+{
+    class BishopPathFinder
+    {
+        static void Main()
+        {
+            string[] dimensions = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            FillTheMatrixDiagonaly(matrix);
+
+            //PrintMatrix(matrix);
+
+            int n = int.Parse(Console.ReadLine());
+
+            int currentRowIndex = rows - 1;
+            int currentColIndex = 0;
+
+            int previousRowIndex = currentRowIndex;
+            int previousColIndex = currentColIndex;
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = commandArgs[0];
+                int moves = int.Parse(commandArgs[1]);
+                int move = 1;
+
+                while (true)
+                {
+                    if (currentRowIndex < 0 || currentRowIndex >= rows || currentColIndex < 0 || currentColIndex >= cols)
+                    {
+                        currentRowIndex = previousRowIndex;
+                        currentColIndex = previousColIndex;
+                        break;
+                    }
+
+                    previousRowIndex = currentRowIndex;
+                    previousColIndex = currentColIndex;
+
+                    sum += matrix[currentRowIndex, currentColIndex];
+                    matrix[currentRowIndex, currentColIndex] = 0;
+
+                    if (move >= moves)
+                    {
+                        break;
+                    }
+
+                    if (direction == "RU" || direction == "UR")
+                    {
+                        currentRowIndex--;
+                        currentColIndex++;
+                    }
+                    else if (direction == "LU" || direction == "UL")
+                    {
+                        currentRowIndex--;
+                        currentColIndex--;
+                    }
+                    else if (direction == "DL" || direction == "LD")
+                    {
+                        currentRowIndex++;
+                        currentColIndex--;
+                    }
+                    else if (direction == "DR" || direction == "RD")
+                    {
+                        currentRowIndex++;
+                        currentColIndex++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid direction.");
+                    }
+
+                    move++;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static void FillTheMatrixDiagonaly(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int diagonalCounter = 1;
+
+            int counter = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int row = rows - 1 - i;
+                int col = 0;
+                while (true)
+                {
+                    if (row >= rows || col >= cols)
+                    {
+                        break;
+                    }
+                    matrix[row, col] = counter;
+                    row++;
+                    col++;
+                }
+                counter += 3;
+                diagonalCounter++;
+            }
+
+            for (int i = 0; i < cols; i++)
+            {
+                int row = 0;
+                int col = i + 1;
+
+                while (true)
+                {
+                    if (row >= rows || col >= cols)
+                    {
+                        break;
+                    }
+                    matrix[row, col] = counter;
+                    row++;
+                    col++;
+                }
+                counter += 3;
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string row = string.Empty;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row += matrix[i, j] + " ";
+                }
+                Console.WriteLine(row.Trim());
+            }
+        }
+    }
+}
