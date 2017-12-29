7e61d1518357364488203a4c70166ce60c93dfc3
Alexander Markov <alexander_29@abv.bg>
2017-12-29 22:59:43 +0200

    Singing Cats solved.

--- AdvancedPractice/5.SingingCats/SingingCats.cs
+++ AdvancedPractice/5.SingingCats/SingingCats.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace _5.SingingCats
 {
@@ -10,23 +9,17 @@ namespace _5.SingingCats
         {
             string c = Console.ReadLine().Split()[0];
             string s = Console.ReadLine().Split()[0];
-            byte catsCount = byte.Parse(c);
-            byte songsCount = byte.Parse(s);
+            int catsCount = int.Parse(c);
+            int songsCount = int.Parse(s);
 
             string command = Console.ReadLine();
 
-            var songsNumber = new Dictionary<byte, byte>(); // song -> number of cats that know the song
-            var songsAndCats = new Dictionary<byte, List<byte>>(); // song -> cats list
-            var catAndSongs = new Dictionary<byte, List<byte>>(); // cat song list
 
-            for (byte song = 0; song < songsCount; song++)
+            var songsAndCats = new Dictionary<int, HashSet<int>>(); // song -> cats list
+
+            for (int i = 0; i < songsCount; i++)
             {
-                songsNumber.Add(song, 0);
-                songsAndCats.Add(song, new List<byte>());
-            }
-            for (byte cat = 0; cat < catsCount; cat++)
-            {
-                catAndSongs.Add(cat, new List<byte>());
+                songsAndCats.Add(i + 1, new HashSet<int>());
             }
 
             while (command != "Mew!")
@@ -36,58 +29,56 @@ namespace _5.SingingCats
                 byte cat = byte.Parse(commandArgs[1]);
                 byte song = byte.Parse(commandArgs[4]);
 
-                songsNumber[(byte)(song - 1)]++;
-                songsAndCats[(byte)(song - 1)].Add((byte)(cat - 1));
-                catAndSongs[(byte)(cat - 1)].Add((byte)(song - 1));
+
+                songsAndCats[song].Add(cat);
+
 
                 command = Console.ReadLine();
             }
 
-            foreach (var item in catAndSongs)
+            for (int i = 1; i <= songsCount; i++)
             {
-                if (item.Value.Count == 0)
+                foreach (var combination in Combinations(i, songsCount))
                 {
-                    Console.WriteLine("No concert!");
-                    return;
-                }
-            }
-            
-            byte catSingig = 0;
-            byte count = 0;
-            while (true)
-            {
-                count++;
-                var sortedSongs = songsNumber.ToList();
-                sortedSongs.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value));
-
-                byte song = sortedSongs[0].Key;
-                HashSet<byte> cats = new HashSet<byte>();
-
-                foreach (var cat in songsAndCats[song])
-                {
-                    cats.Add(cat);
-                    catSingig++;
-                }
-
-                foreach (var cat in cats)
-                {
-                    foreach (var sg in catAndSongs[cat])
+                    var result = new HashSet<int>();
+                    foreach (var song in combination)
                     {
-                        songsNumber[sg]--;
+                        result.UnionWith(songsAndCats[song]);
+                    }
+                    if (result.Count == catsCount)
+                    {
+                        Console.WriteLine(i);
+                        return;
                     }
                 }
-
-                if (catSingig == catsCount)
-                {
-                    break;
-                }
-                if (sortedSongs.Count <= 0)
-                {
-                    break;
-                }
             }
 
-            Console.WriteLine(count);
+            Console.WriteLine("No concert!");
+        }
+
+        public static IEnumerable<int[]> Combinations(int k, int n)
+        {
+            int[] result = new int[k];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+
+                while (value < n)
+                {
+                    result[index++] = ++value;
+                    stack.Push(value);
+
+                    if (index == k)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
